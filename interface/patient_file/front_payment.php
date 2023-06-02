f40c1ff6edddf0b480a2e1a35aa737f1f6f7dc1a
juggernautsei <sherwingaddis@gmail.com>
2023-06-02 13:13:20 -0400

    Removing descriptions of services

--- interface/patient_file/front_payment.php
+++ interface/patient_file/front_payment.php
@@ -649,8 +649,8 @@ function toencounter(enc, datestr, topframe) {
                         <!-- Omega Itemized Invoice -->
                         <tr class="bg-color-w">
                             <!--<th><?php //echo xlt('Description'); ?></th>-->
-                            <th><?php echo xlt('Price'); ?></th>
-                            <th><?php echo xlt('Qty'); ?></th>
+                            <!--<th><?php //echo xlt('Price'); ?></th>-->
+                            <!--<th><?php echo xlt('Qty'); ?></th>-->
                             <th><?php echo xlt('Total'); ?></th>
                         </tr>
                         <!-- Omega Itemized Invoice -->
@@ -667,8 +667,8 @@ function toencounter(enc, datestr, topframe) {
                             ?>
                                 <tr>
                                     <!--<td><?php //echo text($each_row['code_text']); ?></td>-->
-                                    <td><?php //echo text($each_row['fee']); ?></td>
-                                    <td><?php //echo text($each_row['units']); ?></td>
+                                    <!--<td><?php //echo text($each_row['fee']); ?></td>-->
+                                    <!--<td><?php //echo text($each_row['units']); ?></td>-->
                                     <td><?php echo text($each_row['fee'] * $each_row['units']); ?></td>
                                 </tr>
                         <?php } ?>
